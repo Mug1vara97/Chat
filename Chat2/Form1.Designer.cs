7ffea498df2dfe3778689e5476f3d0133e581a1f
Mug1vara97 <mugivarakun97@gmail.com>
2024-12-02 10:37:06 +0300

    Добавьте файлы проекта.

---
+++ Chat2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Chat2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lstMessages = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstActiveUsers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstMessages
+            // 
+            this.lstMessages.FormattingEnabled = true;
+            this.lstMessages.ItemHeight = 15;
+            this.lstMessages.Location = new System.Drawing.Point(12, 12);
+            this.lstMessages.Name = "lstMessages";
+            this.lstMessages.Size = new System.Drawing.Size(360, 199);
+            this.lstMessages.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 217);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(278, 23);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(296, 217);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstActiveUsers
+            // 
+            this.lstActiveUsers.FormattingEnabled = true;
+            this.lstActiveUsers.ItemHeight = 15;
+            this.lstActiveUsers.Location = new System.Drawing.Point(378, 12);
+            this.lstActiveUsers.Name = "lstActiveUsers";
+            this.lstActiveUsers.Size = new System.Drawing.Size(120, 229);
+            this.lstActiveUsers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 261);
+            this.Controls.Add(this.lstActiveUsers);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lstMessages);
+            this.Name = "Form1";
+            this.Text = "Чат - Боб";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ListBox lstMessages;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstActiveUsers;
+    }
+}
---
+++ Chat2/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Chat2
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpClient;
+        private const string MulticastAddress = "224.5.5.5";
+        private const int Port = 5000;
+        private Thread receiveThread;
+        private string userName;
+        private HashSet<string> activeUsers = new HashSet<string>();
+        private System.Windows.Forms.Timer statusTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            userName = "Боб";
+            StartListening();
+            InitializeStatusTimer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SendUserStatus("в сети");
+        }
+
+        private void StartListening()
+        {
+            udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, Port));
+            udpClient.JoinMulticastGroup(IPAddress.Parse(MulticastAddress));
+            receiveThread = new Thread(ReceiveMessages);
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+
+        private void InitializeStatusTimer()
+        {
+            statusTimer = new System.Windows.Forms.Timer();
+            statusTimer.Interval = 1000;
+            statusTimer.Tick += (s, e) => SendUserStatus("в сети");
+            statusTimer.Start();
+        }
+
+        private void ReceiveMessages()
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            while (true)
+            {
+                byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
+                string message = Encoding.UTF8.GetString(receivedBytes);
+                ProcessMessage(message);
+            }
+        }
+
+        private void ProcessMessage(string message)
+        {
+            if (message.StartsWith("в сети: "))
+            {
+                string user = message.Substring(8).Trim();
+                if (activeUsers.Add(user))
+                {
+                    Invoke(new Action(() => UpdateActiveUsersList()));
+                    string displayName = user == userName ? "Вы" : user;
+                    Invoke(new Action(() => lstMessages.Items.Add($"{displayName} подключился.")));
+                }
+            }
+            else if (message.StartsWith("вышел: "))
+            {
+                string user = message.Substring(7).Trim();
+                if (activeUsers.Remove(user))
+                {
+                    Invoke(new Action(() => UpdateActiveUsersList()));
+                    string displayName = user == userName ? "Вы" : user;
+                    Invoke(new Action(() => lstMessages.Items.Add($"{displayName} отключился.")));
+                }
+            }
+            else
+            {
+                string sender = message.Split(':')[0];
+                string displayName = sender == userName ? "Вы" : sender;
+                Invoke(new Action(() => lstMessages.Items.Add($"{displayName}: {message.Substring(sender.Length + 1).Trim()}")));
+            }
+        }
+
+        private void UpdateActiveUsersList()
+        {
+            lstActiveUsers.Items.Clear();
+            foreach (var user in activeUsers)
+            {
+                lstActiveUsers.Items.Add(user);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                string formattedMessage = $"{userName}: {message}";
+                byte[] messageBytes = Encoding.UTF8.GetBytes(formattedMessage);
+                udpClient.Send(messageBytes, messageBytes.Length, MulticastAddress, Port);
+                txtMessage.Clear();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SendUserStatus("вышел");
+            udpClient.DropMulticastGroup(IPAddress.Parse(MulticastAddress));
+            udpClient.Close();
+            receiveThread.Abort();
+            statusTimer.Stop();
+            base.OnFormClosing(e);
+        }
+
+        private void SendUserStatus(string status)
+        {
+            string message = $"{status}: {userName}";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            udpClient.Send(messageBytes, messageBytes.Length, MulticastAddress, Port);
+        }
+    }
+}
